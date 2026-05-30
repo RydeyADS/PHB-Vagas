9c6dd229d7ecdcc7d2b3627f57afa4be4fa2157a
Hemesson Rydey <rydeyads@gmail.com>
2026-05-30 17:43:12 -0300

    feat: projeto inicial PHB Vagas

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import $ from 'jquery';
+
+window.$ = $;
+window.jQuery = $;
+
+console.log('Bootstrap e jQuery carregados');
